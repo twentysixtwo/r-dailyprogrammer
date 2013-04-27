90df87ab38b2ac896ad29deb8913942cbb4b5157
Jon Huang <jonathanxhuang@gmail.com>
2013-04-26 22:24:28 -0400

    Initial commit of Userspace threading

---
+++ UserspaceThreading.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* 
+Challenge #122 [Intermediate] User-Space Threading
+
+Implement efficient and dynamic user-level threading library
+
+efficient - switching thread of execution must be done as quickly as possible
+dynamic - provide a way to allow end-users to change how fast you switch and what kind of algorithm you use for timing
+
+//ThreadID createThread(void (threadFunction)(void)) 
+//bool destroyThread( ThreadID givenThreadID) 
+*/
+
+int main() {
+
+
+return 0;
+}
